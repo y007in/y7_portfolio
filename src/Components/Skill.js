ea310d1734756bae8d6eaf299bf4ab509c8b117a
DESKTOP-R2R34G5\Administrator <ujin4719@naver.com>
2023-08-08 23:04:09 +0900

    skill reset

--- src/Components/Skill.js
+++ src/Components/Skill.js
@@ -2,19 +2,6 @@ import React from "react";
 import "../scss/Skill.scss";
 
 const Skill = ({ skillIcon }) => {
-  const [isMobile, setIsMobile] = useState(window.innerWidth <= 1024);
-  const [showMobileContent, setShowMobileContent] = useState(false);
-
-  const handleResize = () => {
-    setIsMobile(window.innerWidth <= 1024);
-  };
-
-  window.addEventListener("resize", handleResize);
-
-  const toggleMobileContent = () => {
-    setShowMobileContent(!showMobileContent);
-  };
-
   return (
     <div className="skills">
       <div className="skill_container">
@@ -29,16 +16,7 @@ const Skill = ({ skillIcon }) => {
                     <img src={item.icon} />
                   </div>
                   <p className="skill_name">{item.skill}</p>
-                  {isMobile && (
-                    <ul
-                      className={`${styles.skill_des} ${
-                        showMobileContent ? styles["show-mobile"] : ""
-                      }`}
-                      onClick={toggleMobileContent}
-                    >
-                      {item.des}
-                    </ul>
-                  )}
+                  <ul className="skill_des">{item.des}</ul>
                 </div>
               ))}
             </div>
@@ -52,16 +30,7 @@ const Skill = ({ skillIcon }) => {
                     <img src={item.icon} />
                   </div>
                   <p className="skill_name">{item.skill}</p>
-                  {isMobile && (
-                    <ul
-                      className={`${styles.skill_des} ${
-                        showMobileContent ? styles["show-mobile"] : ""
-                      }`}
-                      onClick={toggleMobileContent}
-                    >
-                      {item.des}
-                    </ul>
-                  )}
+                  <ul className="skill_des">{item.des}</ul>
                 </div>
               ))}
             </div>

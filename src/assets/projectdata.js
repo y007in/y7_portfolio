371991f3980d429f855920584d6f92e0e382777e
yoojin <yoojin4719@gmail.com>
2023-08-10 20:28:40 +0900

    data

--- src/assets/projectdata.js
+++ src/assets/projectdata.js
@@ -31,9 +31,9 @@ export const ProjectData = [
     ),
     func: (
       <>
-        <li>- 산후조리원코드로 임산부들만 회원가입 가능합니다.</li>
-        <li>- 산후조리원코드로 임산부들만 회원가입 가능합니다.</li>
-        <li>- 산후조리원코드로 임산부들만 회원가입 가능합니다.</li>
+        <li>- 반응형 웹으로 제작</li>
+        <li>- 각 상품 상세 페이지 별 주문 문품과 수량에 따른 가격 변화</li>
+        <li>- 공동구매 페이지 공동구매 기능</li>
       </>
     ),
   },
@@ -83,126 +83,6 @@ export const ProjectData = [
     skill: (
       <>
         <li>React</li>
-      </>
-    ),
-    func: (
-      <>
-        <li>-산후조리원코드로 임산부들만 회원가입 가능합니다.</li>
-        <li>-산후조리원코드로 임산부들만 회원가입 가능합니다.</li>
-        <li>-산후조리원코드로 임산부들만 회원가입 가능합니다.,</li>
-      </>
-    ),
-  },
-  {
-    id: 4,
-    name: "허그맘(Hugmom)",
-    team: "팀 프로젝트",
-    image: `${hugmom}`,
-    des: "많은 산모들이 산후조리원을 이용하는 추세이며 산모들이 신선한 식품과 좋은 제품들을 찾는다고 하여 산후조리원에 있는 산모들 전용 쇼핑몰인 허그맘을 제작",
-    github: "https://github.com/That-s-great/Hugmom",
-    url: "https://hugmom-b6187.web.app/html/01-intro_logout.html",
-    date: "2023.05.30 - 2023.06.30",
-    skill: (
-      <>
-        <li>Html5</li>
-        <li>CSS3</li>
-        <li>JavaScript</li>
-      </>
-    ),
-    func: (
-      <>
-        <li>-산후조리원코드로 임산부들만 회원가입 가능합니다.</li>
-        <li>-산후조리원코드로 임산부들만 회원가입 가능합니다.</li>
-        <li>-산후조리원코드로 임산부들만 회원가입 가능합니다.,</li>
-      </>
-    ),
-  },
-  {
-    id: 4,
-    name: "허그맘(Hugmom)",
-    team: "팀 프로젝트",
-    image: `${hugmom}`,
-    des: "많은 산모들이 산후조리원을 이용하는 추세이며 산모들이 신선한 식품과 좋은 제품들을 찾는다고 하여 산후조리원에 있는 산모들 전용 쇼핑몰인 허그맘을 제작",
-    github: "https://github.com/That-s-great/Hugmom",
-    url: "https://hugmom-b6187.web.app/html/01-intro_logout.html",
-    date: "2023.05.30 - 2023.06.30",
-    skill: (
-      <>
-        <li>Html5</li>
-        <li>CSS3</li>
-        <li>JavaScript</li>
-      </>
-    ),
-    func: (
-      <>
-        <li>-산후조리원코드로 임산부들만 회원가입 가능합니다.</li>
-        <li>-산후조리원코드로 임산부들만 회원가입 가능합니다.</li>
-        <li>-산후조리원코드로 임산부들만 회원가입 가능합니다.,</li>
-      </>
-    ),
-  },
-  {
-    id: 4,
-    name: "허그맘(Hugmom)",
-    team: "팀 프로젝트",
-    image: `${hugmom}`,
-    des: "많은 산모들이 산후조리원을 이용하는 추세이며 산모들이 신선한 식품과 좋은 제품들을 찾는다고 하여 산후조리원에 있는 산모들 전용 쇼핑몰인 허그맘을 제작",
-    github: "https://github.com/That-s-great/Hugmom",
-    url: "https://hugmom-b6187.web.app/html/01-intro_logout.html",
-    date: "2023.05.30 - 2023.06.30",
-    skill: (
-      <>
-        <li>Html5</li>
-        <li>CSS3</li>
-        <li>JavaScript</li>
-      </>
-    ),
-    func: (
-      <>
-        <li>-산후조리원코드로 임산부들만 회원가입 가능합니다.</li>
-        <li>-산후조리원코드로 임산부들만 회원가입 가능합니다.</li>
-        <li>-산후조리원코드로 임산부들만 회원가입 가능합니다.,</li>
-      </>
-    ),
-  },
-  {
-    id: 4,
-    name: "허그맘(Hugmom)",
-    team: "팀 프로젝트",
-    image: `${hugmom}`,
-    des: "많은 산모들이 산후조리원을 이용하는 추세이며 산모들이 신선한 식품과 좋은 제품들을 찾는다고 하여 산후조리원에 있는 산모들 전용 쇼핑몰인 허그맘을 제작",
-    github: "https://github.com/That-s-great/Hugmom",
-    url: "https://hugmom-b6187.web.app/html/01-intro_logout.html",
-    date: "2023.05.30 - 2023.06.30",
-    skill: (
-      <>
-        <li>Html5</li>
-        <li>CSS3</li>
-        <li>JavaScript</li>
-      </>
-    ),
-    func: (
-      <>
-        <li>-산후조리원코드로 임산부들만 회원가입 가능합니다.</li>
-        <li>-산후조리원코드로 임산부들만 회원가입 가능합니다.</li>
-        <li>-산후조리원코드로 임산부들만 회원가입 가능합니다.,</li>
-      </>
-    ),
-  },
-  {
-    id: 4,
-    name: "허그맘(Hugmom)",
-    team: "팀 프로젝트",
-    image: `${hugmom}`,
-    des: "많은 산모들이 산후조리원을 이용하는 추세이며 산모들이 신선한 식품과 좋은 제품들을 찾는다고 하여 산후조리원에 있는 산모들 전용 쇼핑몰인 허그맘을 제작",
-    github: "https://github.com/That-s-great/Hugmom",
-    url: "https://hugmom-b6187.web.app/html/01-intro_logout.html",
-    date: "2023.05.30 - 2023.06.30",
-    skill: (
-      <>
-        <li>Html5</li>
-        <li>CSS3</li>
-        <li>JavaScript</li>
       </>
     ),
     func: (

6bd201db3bf07559339ac668d9723bbb4802ac61
DESKTOP-R2R34G5\Administrator <ujin4719@naver.com>
2023-08-28 00:25:58 +0900

    intro

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -2,10 +2,9 @@ import React, { useRef, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "../scss/intro.scss";
 import { intros } from "../assets/projectdata";
-import { motion } from "framer-motion";
+import { FaAnglesDown } from "react-icons/fa6";
 
 const Intro = () => {
-  const [selectedIntroIndex, setSelectedIntroIndex] = useState(null);
   const [scrollOffset, setScrollOffset] = useState(0);
   const navigate = useNavigate();
   const moveToSkill = useRef();
@@ -14,9 +13,6 @@ const Intro = () => {
       behavior: "smooth",
       block: "nearest",
     });
-  };
-  const handleHashtagClick = (index) => {
-    setSelectedIntroIndex((prevIndex) => (prevIndex === index ? null : index));
   };
 
   useEffect(() => {
@@ -35,59 +31,39 @@ const Intro = () => {
     <div className="intro">
       <div className="intro_container">
         <div className="hi">
-          <div className="hashtag">
-            {intros.map((intro, index) => (
-              <span
-                key={index}
-                onClick={() => handleHashtagClick(index)}
-                /*className={selectedIntroIndex === index ? "active" : null}*/
-              >
-                {intros[index].hashtag}
-              </span>
-            ))}
+          <span>안녕하세요.</span>
+          <div
+            style={{
+              transform: `translateX(-${scrollOffset}px)`,
+              marginTop: "10px",
+              lineHeight: "110%",
+            }}
+          >
+            <div className="hashtag">
+              {intros.map((intro, index) => (
+                <span key={index}>{intros[index].hashtag}</span>
+              ))}
+            </div>
+            <div className="front-end">프론트엔드 개발자</div>
           </div>
-          <h1>
-            안녕하세요. <br />
-            <div style={{ transform: `translateX(-${scrollOffset}px)` }}>
-              <span className="front-end">프론트엔드 개발자</span>
-            </div>
-            <div style={{ transform: `translateX(${scrollOffset}px)` }}>
-              <span className="intro_name">장유진</span>입니다.
-            </div>
-          </h1>
+          <div style={{ transform: `translateX(${scrollOffset}px)` }}>
+            <span className="intro_name">장유진</span>입니다.
+          </div>
         </div>
-        {/* {selectedIntroIndex !== null && (
-          <div className="introduce">
-            <article className="container project">
-              <header>
-                <div className="circle_wrap">
-                  <div className="circle red"></div>
-                  <div className="circle yellow"></div>
-                  <div className="circle green"></div>
-                </div>
-                <div className="title">
-                  {intros[selectedIntroIndex].hashtag}
-                </div>
-              </header>
-              <div className="content_project">
-                {intros[selectedIntroIndex].hashtagDetail}
-              </div>
-            </article>
-          </div>
-        )} */}
-      </div>
-      <div className="btns">
-        <button className="moveBtn" onClick={onMoveToSkill}>
-          더 알아보기
-        </button>
-        <button
-          className="moveBtn"
-          onClick={() => {
-            navigate("/project");
-          }}
-        >
-          프로젝트 보러가기
-        </button>
+        {/* <div className="btns">
+          <button className="moveBtn" onClick={onMoveToSkill}>
+            더 알아보기
+          </button>
+          <button
+            className="moveBtn"
+            onClick={() => {
+              navigate("/project");
+            }}
+          >
+            프로젝트 보러가기
+          </button>
+        </div> */}
+        <FaAnglesDown />
       </div>
     </div>
   );

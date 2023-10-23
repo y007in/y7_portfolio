4b666fe7fc02c692d93d7700f4261d721e615f6b
yoojin <yoojin4719@gmail.com>
2023-10-23 16:49:21 +0900

    scrolltop fix

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.scss";
 import App from "./App";
 
 import { BrowserRouter } from "react-router-dom";
-import ScrollToTop from "./components/ScrollTop";
+import ScrollToTop from "./components/ScrollToTop";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(

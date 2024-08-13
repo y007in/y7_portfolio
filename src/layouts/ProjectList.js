276f1c16fafccd3452025145c8d1ad909456b45f
yoojin <yoojin4719@gmail.com>
2024-08-13 16:38:19 +0900

    REFACTOR : 프로젝트 index수정

--- src/layouts/ProjectList.js
+++ src/layouts/ProjectList.js
@@ -13,14 +13,20 @@ const ProjectList = ({ data }) => {
           <p>* 썸네일 클릭 시 자세한 내용을 볼 수 있습니다.</p>
         </header>
         <article className="projects">
-          {data.slice(0, data.length - 2).map((item) => (
-            <ProjectThumb key={item.id} {...item} />
-          ))}
+          {[...data]
+            .reverse()
+            .slice(0, data.length - 2)
+            .map((item) => (
+              <ProjectThumb key={item.id} {...item} />
+            ))}
           {viewMore ? (
             <>
-              {data.slice(-2).map((item) => (
-                <ProjectThumb key={item.id} {...item} />
-              ))}
+              {[...data]
+                .reverse()
+                .slice(-2)
+                .map((item) => (
+                  <ProjectThumb key={item.id} {...item} />
+                ))}
             </>
           ) : (
             <button className="viewMore" onClick={() => setViewMore(!viewMore)}>
